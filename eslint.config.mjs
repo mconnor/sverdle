3e1cecd15097ca41dfdc12d46b0d48dd0e91e959
mike connor <mike@mikeconnor.tech>
2025-01-08 14:26:10 -0600

    linting fuss, @sveltejs/adapter-vercel'

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,28 +5,38 @@ import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
+import svelteParser from 'svelte-eslint-parser';
+
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+
+export const extraFileExtensions = ['.svelte', '.ts'];
 
 export default ts.config(
 	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs['flat/recommended'],
-	prettier,
 	...svelte.configs['flat/prettier'],
+	prettier,
 	{
 		languageOptions: {
 			globals: {
 				...globals.browser,
 				...globals.node
+			},
+
+			parserOptions: {
+				extraFileExtensions,
+				parser: ts.parser
 			}
 		}
 	},
 	{
 		files: ['**/*.svelte'],
-
 		languageOptions: {
+			parser: svelteParser,
 			parserOptions: {
+				extraFileExtensions,
 				parser: ts.parser
 			}
 		}
